c32834ae9053b16973c40fccf14f2fac9fd3f22b
vally10@live.com <vally10@live.com>
2025-07-24 17:29:18 +0200

    chore: Add SwiftLint

---
+++ scripts/run-swiftlint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Run SwiftLint in Xcode
+if which swiftlint >/dev/null; then
+  swiftlint
+else
+  echo "warning: SwiftLint not installed, download from https://github.com/realm/SwiftLint"
+fi
